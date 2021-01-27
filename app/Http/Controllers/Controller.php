641462881f7e503197d207e6bc960d5007dd4ebb
Jeferson Oliveira <zuruckyt@gmail.com>
2021-01-26 23:50:13 -0300

    WIP: Swagger Documentation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,9 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(title="Cars API", version="1.0")
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
