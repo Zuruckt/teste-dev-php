da09642e202a6cbf12aa61087dc549cbd51efbad
Jeferson Oliveira <zuruckyt@gmail.com>
2021-01-26 23:44:38 -0300

    Add backend routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CarController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/carros', [CarController::class, 'index']);
+Route::post('/carros', [CarController::class, 'store']);
+Route::get('/carros/{id}', [CarController::class, 'show']);
+Route::put('/carros/{id}', [CarController::class, 'update']);
+Route::delete('/carros/{id}', [CarController::class, 'delete']);
