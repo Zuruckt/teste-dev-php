4c5a6c8be997cfa7d19b78f0e9e78225d58f429e
Jeferson Oliveira <zuruckyt@gmail.com>
2021-01-27 22:18:23 -0300

    Remove unused import

--- app/Http/Controllers/Api/CarController.php
+++ app/Http/Controllers/Api/CarController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Car;
 use App\Traits\RespondsApi;
 use Illuminate\Http\Request;
 use App\Repositories\CarRepository;
