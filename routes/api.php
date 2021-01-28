076497cc697718385c96483f2f0b775547b1f08b
Jeferson Oliveira <zuruckyt@gmail.com>
2021-01-27 22:15:48 -0300

    Add route groups

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,12 @@ use App\Http\Controllers\Api\CarController;
 |
 */
 
-Route::get('/carros', [CarController::class, 'index']);
-Route::post('/carros', [CarController::class, 'store']);
-Route::get('/carros/{id}', [CarController::class, 'show']);
-Route::put('/carros/{id}', [CarController::class, 'update']);
-Route::delete('/carros/{id}', [CarController::class, 'delete']);
+
+Route::group(['prefix' => 'cars'], function () {
+    Route::get('/', [CarController::class, 'index']);
+    Route::post('/', [CarController::class, 'store']);
+    Route::get('/{id}', [CarController::class, 'show']);
+    Route::put('/{id}', [CarController::class, 'update']);
+    Route::delete('/{id}', [CarController::class, 'delete']);
+});
+
